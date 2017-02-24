971a6881637e42874139865fecf96b56100212a4
Nicholas Roberts <nicholas.roberts.au@gmail.com>
2017-02-25 01:41:39 +1100

    Completed example publish, draft of REST subscribe endpoint

--- main.go
+++ main.go
@@ -3,102 +3,95 @@ package main
 import (
     "fmt"
     "math/rand"
+    "net/http"
     // "io/ioutil"
     // "reflect"
     // "sync"
     "github.com/gin-gonic/gin"
     // "time"
-    "github.com/NaySoftware/fcm"
+    "github.com/NaySoftware/go-fcm"
     "strconv"
+    "log"
     "github.com/robfig/cron"
 )
 
 const (
      serverKey = "YOUR-KEY"
-     rootTopic = "foo-topic"
+     rootTopic = "/topics/fooTopic"
      MIN_THRESHOLD=0
      MAX_THRESHOLD=16
 )
 
+var client = &http.Client{}
+
+
+type Subscription struct {
+    Topic   string `form:"topic" json:"topic" binding:"required"`
+    InstanceId  string `form:"instance_id" json:"instance_id" binding:"required"`
+}
+
 // construct funcs for each interval
 // publishes to root_topic//stream/hour
-func mkPub(utc_hour int, stream string, root_topic string) func() {
-  topic := root_topic + "/" + strconv.Itoa(utc_hour)
+func mkPub(utc_hour int, root_topic string) func() {
+  topic := root_topic + "-" + strconv.Itoa(utc_hour)
   return func() {
-    
+
     c := fcm.NewFcmClient(serverKey)
     r := rand.New(rand.NewSource(99))
-    data := r.Int31()
-    x := topic + " + " + strconv.Itoa(data.toInt())
-    fmt.Println(x)
+    random_num := r.Int31()
+    data := map[string]string{
+        "random_num": strconv.Itoa(int(random_num)),
+    }
     c.NewFcmMsgTo(topic, data)
 
     status, err := c.Send()
 
     if err == nil {
-    status.PrintResults()
+        status.PrintResults()
     } else {
         fmt.Println(err)
     }
   }
 }
 
-func PostUser(c *gin.Context) {
-    // The futur code…
+func SubscribeToTopic(c *gin.Context) {
+    var json Subscription
+    if c.BindJSON(&json) == nil {
+        // build the request object
+        req, err := http.NewRequest("GET",
+            "https://iid.googleapis.com/iid/v1/" +
+            json.InstanceId+"/rel/topics/"+json.Topic, nil)
+        req.Header.Add("Authorization", "key="+serverKey)
+        req.Header.Add("Content-Type", "application/json")
+        req.Header.Add("Content-Length", "0")
+        resp, err := client.Do(req)
+        if err != nil {
+          fmt.Println(err)
+        } else {
+            fmt.Println(resp)
+        }
+        c.String(200, "hello")
+        log.Println("hallo")
+        defer resp.Body.Close()
+    }
 }
 
 
 
 func main() {
-  r := gin.Default()
+  r := gin.New()
+  r.Use(gin.Logger())
+  r.Use(gin.Recovery())
   c := cron.New()
   for i := 0; i < 24; i++ {
-    test := mkPub(i, rootTopic)
-    c.AddFunc("0 0 " + strconv.Itoa(i) + " * * *", test)
+    // test := mkPub(i, rootTopic)
+    // c.AddFunc("" + strconv.Itoa(i) + " * * * * *", test)
   }
   c.AddFunc("@every 10s",      func() { fmt.Println("Every 10s") })
   v1 := r.Group("api/v1")
   {
-      v1.POST("/users", PostUser)
-      v1.GET("/users", PostUser)
-      v1.GET("/users/:id", PostUser)
-      v1.PUT("/users/:id", PostUser)
-      v1.DELETE("/users/:id", PostUser)
+      v1.POST("/subscribe", SubscribeToTopic)
   }
   c.Start()
   r.Run(":8080")
-  
-  // resp, err := http.Get("http://www.arpansa.gov.au/uvindex/realtime/xml/uvvalues.xml")
-  // if err != nil {
-  //   fmt.Println(err)
-  // }
-  // defer resp.Body.Close()
-
-  // var q Arpansa_Query;
-  // body, err := ioutil.ReadAll(resp.Body)
-
-  // if err != nil {
-  //   panic(err.Error())
-  // }
-
-  // fmt.Println(reflect.TypeOf(body))
-  // fmt.Println(string(body))
-	// xml.Unmarshal(body, &q)
-  // fmt.Println(q)
-
-  // messages := make(chan int)
-  // var wg sync.WaitGroup
-  // wg.Add(1)
-  // go func() {
-  //   defer wg.Done()
-  //   publish_worker(0)
-  //   messages <- 1
-  // }()
-
-  // go func() {
-  //     for i := range messages {
-  //         fmt.Println(i)
-  //     }
-  // }()
-  // wg.Wait()
 }
